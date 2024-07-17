1b1876db38ff595617ddd147e74e92ffd226c71f
vprint <valentin.printemps@gmail.com>
2024-07-17 20:28:09 +0200

    Passage des mesures sur un store

--- src/components/MeasureComponent/MeasureComponent.vue
+++ src/components/MeasureComponent/MeasureComponent.vue
@@ -1,127 +1,17 @@
 <script setup lang="ts">
 // Vue/Quasar imports
-import { Ref, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 
 // Store imports
-import { useMapInteractionStore } from 'src/stores/map-interaction-store';
+import { useMeasureStore } from 'src/stores/measure-store';
 
 // Map imports
-import { Geometry, LineString, Polygon } from 'ol/geom';
-import { unByKey } from 'ol/Observable';
 
 // Others imports
-import {
-  IMeasureType,
-  MeasureEventType,
-  MeasureStartEvent,
-} from 'src/plugins/MeasurePlugin';
-import { INTERACTIONS_PARAMS } from 'src/utils/params/interactionsParams';
 
 // Script
-
-const { measurePlugin } = storeToRefs(useMapInteractionStore());
-const { enableInteraction } = useMapInteractionStore();
-const formatedMeasure: Ref<string> = ref('');
-const measure: Ref<number> = ref(0);
-const measureMenu = ref(false);
-
-/**
- * Manaage measure activation / deactivation
- */
-function manageMeasure(mode: IMeasureType): void {
-  measurePlugin.value.setActive(true);
-  measurePlugin.value.addMeasure(mode);
-  enableInteraction(INTERACTIONS_PARAMS.selector, false);
-}
-
-/**
- * Open menu and set selection mode to true
- */
-function manageMenu(): void {
-  measurePlugin.value.removeMeasure();
-  enableInteraction(INTERACTIONS_PARAMS.selector, true);
-}
-
-/**
- * Calculate measure for a given polygon
- * @param geom Input geometry
- */
-function calculateMeasure(geom: Geometry): void {
-  // Calculate area if geometry is a polygon
-  if (geom instanceof Polygon) {
-    formatedMeasure.value = formatArea(geom);
-    measure.value = geom.getArea();
-  }
-  // Calculate length if geometry is a line
-  else if (geom instanceof LineString) {
-    formatedMeasure.value = formatLength(geom);
-    measure.value = geom.getLength();
-  }
-}
-
-/**
- * Format length output.
- * @param line The line
- * @returns The formatted length.
- */
-function formatLength(line: LineString): string {
-  const length = line.getLength();
-  let output: string;
-
-  if (length > 100) {
-    output = `${Math.round((length / 1000) * 100) / 100} km`;
-  } else {
-    output = `${Math.round(length * 100) / 100} m`;
-  }
-  return output;
-}
-
-/**
- * Format area output
- * @param polygon The polygone
- * @returns Formatted area
- */
-function formatArea(polygon: Polygon): string {
-  const area = polygon.getArea();
-  let output: string;
-
-  if (area > 10000) {
-    output = `${Math.round((area / 1000000) * 100) / 100} km²`;
-  } else {
-    output = `${Math.round(area * 100) / 100} m²`;
-  }
-  return output;
-}
-
-/**
- * Manage measure start event
- */
-const onMeasureStart = measurePlugin.value.on(
-  // @ts-expect-error - Type problems due to typescript / ol
-  MeasureEventType.MEASURE_START,
-  (e: MeasureStartEvent) => {
-    e.feature.getGeometry()?.on('change', (evt) => {
-      const geom = evt.target as Geometry;
-      calculateMeasure(geom);
-    });
-  }
-);
-
-/**
- * Manage measure end event and remove listeners
- */
-measurePlugin.value.on(
-  // @ts-expect-error - Type problems due to typescript / ol
-  MeasureEventType.MEASURE_END,
-  () => {
-    measurePlugin.value.removeMeasure();
-    formatedMeasure.value = '0';
-    enableInteraction(INTERACTIONS_PARAMS.selector, true);
-
-    unByKey(onMeasureStart);
-  }
-);
+const { addMeasure, removeMeasure } = useMeasureStore();
+const { measureMenu } = storeToRefs(useMeasureStore());
 </script>
 
 <template>
@@ -133,7 +23,7 @@ measurePlugin.value.on(
     icon="sym_s_straighten"
     class="icon-weight-thin"
     :text-color="$q.platform.is.mobile ? 'primary' : undefined"
-    @click="manageMenu"
+    @click="removeMeasure"
   >
     <q-menu
       v-model="measureMenu"
@@ -145,7 +35,7 @@ measurePlugin.value.on(
           v-close-popup
           clickable
           class="icon-weight-thin"
-          @click="manageMeasure('LineString')"
+          @click="addMeasure('LineString')"
         >
           <q-item-section>
             <q-icon name="straighten" />
@@ -162,7 +52,7 @@ measurePlugin.value.on(
           </q-tooltip>
         </q-item>
 
-        <q-item v-close-popup clickable @click="manageMeasure('Polygon')">
+        <q-item v-close-popup clickable @click="addMeasure('Polygon')">
           <q-item-section>
             <q-icon name="square_foot" />
           </q-item-section>
--- src/stores/measure-store.ts
+++ src/stores/measure-store.ts
@@ -1,7 +1,10 @@
 // Map import
+import { Geometry, LineString, Polygon } from 'ol/geom';
+import { unByKey } from 'ol/Observable';
 
 // Vue/Quasar imports
 import { defineStore, storeToRefs } from 'pinia';
+import { ref, Ref } from 'vue';
 
 // Store imports
 import { useMapInteractionStore } from './map-interaction-store';
@@ -9,7 +12,11 @@ import { useMapInteractionStore } from './map-interaction-store';
 // Others imports
 
 // Interface imports
-import { IMeasureType } from 'src/plugins/MeasurePlugin';
+import {
+  IMeasureType,
+  MeasureEventType,
+  MeasureStartEvent,
+} from 'src/plugins/MeasurePlugin';
 
 // Enum imports
 import { INTERACTIONS_PARAMS } from 'src/utils/params/interactionsParams';
@@ -22,17 +29,113 @@ import { INTERACTIONS_PARAMS } from 'src/utils/params/interactionsParams';
 export const useMeasureStore = defineStore('measureStore', () => {
   const { enableInteraction } = useMapInteractionStore();
   const { measurePlugin } = storeToRefs(useMapInteractionStore());
+  const measure: Ref<number> = ref(0);
+  const formatedMeasure: Ref<string> = ref('');
+  const measureMenu = ref(false);
 
   /**
    * Manaage measure activation / deactivation
+   * @param mode Measure mode - can be either Polygon or LineString
    */
-  function manageMeasure(mode: IMeasureType): void {
+  function addMeasure(mode: IMeasureType): void {
     measurePlugin.value.setActive(true);
     measurePlugin.value.addMeasure(mode);
     enableInteraction(INTERACTIONS_PARAMS.selector, false);
   }
 
+  /**
+   * Remove measure and activate selector
+   */
+  function removeMeasure(): void {
+    measurePlugin.value.removeMeasure();
+    enableInteraction(INTERACTIONS_PARAMS.selector, true);
+  }
+
+  /**
+   * Calculate measure for a given polygon
+   * @param geom Input geometry
+   */
+  function calculateMeasure(geom: Geometry): void {
+    // Calculate area if geometry is a polygon
+    if (geom instanceof Polygon) {
+      formatedMeasure.value = formatArea(geom);
+      measure.value = geom.getArea();
+    }
+    // Calculate length if geometry is a line
+    else if (geom instanceof LineString) {
+      formatedMeasure.value = formatLength(geom);
+      measure.value = geom.getLength();
+    }
+  }
+
+  /**
+   * Format length output.
+   * @param line The line
+   * @returns The formatted length.
+   */
+  function formatLength(line: LineString): string {
+    const length = line.getLength();
+    let output: string;
+
+    if (length > 100) {
+      output = `${Math.round((length / 1000) * 100) / 100} km`;
+    } else {
+      output = `${Math.round(length * 100) / 100} m`;
+    }
+    return output;
+  }
+
+  /**
+   * Format area output
+   * @param polygon The polygone
+   * @returns Formatted area
+   */
+  function formatArea(polygon: Polygon): string {
+    const area = polygon.getArea();
+    let output: string;
+
+    if (area > 10000) {
+      output = `${Math.round((area / 1000000) * 100) / 100} km²`;
+    } else {
+      output = `${Math.round(area * 100) / 100} m²`;
+    }
+    return output;
+  }
+
+  /**
+   * Manage measure start event
+   */
+  const onMeasureStart = measurePlugin.value.on(
+    // @ts-expect-error - Type problems due to typescript / ol
+    MeasureEventType.MEASURE_START,
+    (e: MeasureStartEvent) => {
+      e.feature.getGeometry()?.on('change', (evt) => {
+        const geom = evt.target as Geometry;
+        calculateMeasure(geom);
+      });
+    }
+  );
+
+  /**
+   * Manage measure end event and remove listeners
+   */
+  measurePlugin.value.on(
+    // @ts-expect-error - Type problems due to typescript / ol
+    MeasureEventType.MEASURE_END,
+    () => {
+      measurePlugin.value.removeMeasure();
+      formatedMeasure.value = '0';
+      enableInteraction(INTERACTIONS_PARAMS.selector, true);
+
+      unByKey(onMeasureStart);
+    }
+  );
+
   return {
-    manageMeasure,
+    addMeasure,
+    removeMeasure,
+    calculateMeasure,
+    formatedMeasure,
+    measureMenu,
   };
 });
