aeef28ef4b3eb8bc2827244b999af0e7e1651aef
vprint <valentin.printemps@gmail.com>
2024-07-18 21:20:02 +0200

    Ajout gestion overlay

--- src/components/OverlayComponent/OverlayComponent.vue
+++ src/components/OverlayComponent/OverlayComponent.vue
@@ -7,37 +7,22 @@ import { storeToRefs } from 'pinia';
 
 // Store imports
 import { useMapOverlayStore } from 'src/stores/map-overlay-store';
-import { useMapStore } from 'src/stores/map-store';
 
 // Map imports
-
-import { Overlay } from 'ol';
 
 // Others imports
 
 // Script
-const { map } = storeToRefs(useMapStore());
-const { isVisible } = storeToRefs(useMapOverlayStore());
+const { isVisible, title, content } = storeToRefs(useMapOverlayStore());
 const { setOverlay } = useMapOverlayStore();
 
 let divOverlay: HTMLElement | null;
-let overlay: Overlay;
 
 onMounted(() => {
   divOverlay = document.querySelector('#popup');
-
   if (divOverlay) {
-    overlay = new Overlay({
-      element: divOverlay,
-      offset: [15, 15],
-    });
+    setOverlay(divOverlay);
   }
-
-  setOverlay(overlay);
-
-  map.value.on('pointermove', (e) => {
-    overlay.setPosition(e.coordinate);
-  });
 });
 
 watch(
@@ -59,16 +44,19 @@ watch(
 <template>
   <!-- Overlay -->
   <q-card id="popup" class="overlay-box">
-    <q-card-section class="bg-secondary text-white"> YOLO </q-card-section>
+    <q-card-section class="bg-accent text-white">
+      {{ title }}
+    </q-card-section>
 
-    <q-card-section class="overlay-info"> CONTENT </q-card-section>
+    <q-card-section class="overlay-info"> {{ content }} </q-card-section>
   </q-card>
 </template>
 
 <style lang="scss">
 .overlay-box {
   z-index: 1000;
-  border-radius: 0px;
+  min-width: 100px;
+  border-radius: 2px;
   background-color: rgba(255, 255, 255, 0);
   display: none;
 }
--- src/stores/map-overlay-store.ts
+++ src/stores/map-overlay-store.ts
@@ -4,9 +4,9 @@ import { Overlay } from 'ol';
 // Vue/Quasar imports
 import { defineStore, storeToRefs } from 'pinia';
 import { Ref, ref } from 'vue';
-import { useMapStore } from './map-store';
 
 // Store imports
+import { useMapStore } from './map-store';
 
 // Others imports
 
@@ -16,17 +16,31 @@ import { useMapStore } from './map-store';
  * Manage map overlay
  */
 export const useMapOverlayStore = defineStore('mapOverlay', () => {
+  const defaultOverlay: Ref<HTMLElement | null> = ref(null);
   const isVisible = ref(false);
   const { map } = storeToRefs(useMapStore());
   const overlay: Ref<Overlay | undefined> = ref(undefined);
+  const title = ref('Measure');
+  const content = ref('Click to start drawing');
 
   /**
    * Set the working overlay
    * @param newOverlay Overlay
    */
-  function setOverlay(newOverlay: Overlay): void {
-    overlay.value = newOverlay;
-    map.value.addOverlay(newOverlay);
+  function setOverlay(htmlElement: HTMLElement): void {
+    defaultOverlay.value = htmlElement;
+
+    const _overlay = new Overlay({
+      element: htmlElement,
+      offset: [15, 15],
+    });
+
+    map.value.addOverlay(_overlay);
+    overlay.value = _overlay;
+
+    map.value.on('pointermove', (e) => {
+      overlay.value?.setPosition(e.coordinate);
+    });
   }
 
   /**
@@ -37,10 +51,54 @@ export const useMapOverlayStore = defineStore('mapOverlay', () => {
     isVisible.value = visible;
   }
 
+  /**
+   * Set a new title
+   * @param _title New title
+   */
+  function setTitle(_title: string): void {
+    title.value = _title;
+  }
+
+  /**
+   * Set a new content
+   * @param _content New content
+   */
+  function setContent(_content: string): void {
+    content.value = _content;
+  }
+
+  /**
+   * Reinitialize overlay values.
+   */
+  function reinitializeOverlay(): void {
+    title.value = 'Measure';
+    content.value = 'Click to start drawing';
+  }
+
+  /**
+   * This function create an overlay for a given html template
+   * @param htmlElement Overlay template
+   * @returns Overlay
+   */
+  function createOverlay(htmlElement?: HTMLElement): Overlay {
+    const overlay = new Overlay({
+      element: htmlElement ? htmlElement : defaultOverlay.value!,
+      offset: [15, 15],
+    });
+
+    return overlay;
+  }
+
   return {
     isVisible,
     overlay,
+    title,
+    content,
     setOverlayVisibility,
     setOverlay,
+    setTitle,
+    setContent,
+    reinitializeOverlay,
+    createOverlay,
   };
 });
