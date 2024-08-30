45486f413a2ebb700bd177018ba3d57688b61837
vprint <valentin.printemps@gmail.com>
2024-08-30 18:03:49 +0200

    [Locator] General update

--- src/components/LocatorComponent/LocatorComponent.vue
+++ src/components/LocatorComponent/LocatorComponent.vue
@@ -15,6 +15,7 @@ import { storeToRefs } from 'pinia';
 // Script
 const { map } = storeToRefs(useMapStore());
 const isTrackingEnabled = ref(false);
+const islocationFound = ref(false);
 const location = new Location(INTERACTIONS_PARAMS.location);
 
 map.value.addInteraction(location);
@@ -22,19 +23,22 @@ map.value.addInteraction(location);
 function setTracking(tracking: boolean): void {
   location.enableTracking(tracking);
   isTrackingEnabled.value = tracking;
+  islocationFound.value = false;
 }
+
+// @ts-expect-error normal
+location.on('location:found', () => (islocationFound.value = true));
 </script>
 
 <template>
   <q-btn
+    class="icon-weight-thin"
     :flat="$q.platform.is.desktop"
     :fab="$q.platform.is.desktop"
     :round="$q.platform.is.mobile"
     :square="$q.platform.is.desktop"
-    :color="$q.platform.is.mobile ? 'secondary' : undefined"
-    :icon="isTrackingEnabled ? 'sym_s_location_off' : 'sym_s_location_on'"
-    class="icon-weight-thin"
-    :text-color="$q.platform.is.mobile ? 'primary' : undefined"
+    :icon="isTrackingEnabled ? 'location_on' : 'sym_s_location_on'"
+    :loading="!islocationFound && isTrackingEnabled"
     @click="setTracking(!isTrackingEnabled)"
   >
   </q-btn>
--- src/plugins/Location.ts
+++ src/plugins/Location.ts
@@ -7,47 +7,46 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { easeOut } from 'ol/easing';
 import { Point } from 'ol/geom';
+import { EventsKey } from 'ol/events';
+import { unByKey } from 'ol/Observable';
+import Event from 'ol/events/Event.js';
 
 /**
- * This class add a location tracker to the map. The user
+ * Enum of all location events.
+ */
+export enum LocationEventsType {
+  LOCATION_FOUND = 'location:found',
+  LOCATION_ERROR = 'location:error',
+}
+
+/**
+ * Location events manager.
+ */
+export class LocationEvents extends Event {}
+
+/**
+ * This class add a location tracker to the map.
+ * The user can set the position and accuracy style.
  */
 class Location extends Interaction {
   public trackingEnabled = false;
+  private hasZoomedToLocation = false;
 
   private positionFeature: Feature | undefined;
   private accuracyFeature: Feature | undefined;
-  private hasZoomedToLocation = false;
 
-  private geolocation = new Geolocation({
-    trackingOptions: {
-      enableHighAccuracy: true,
-    },
-    projection: this.getMap()?.getView().getProjection(),
-  });
+  private geolocation: Geolocation;
+  private locationLayer: VectorLayer;
 
-  private locationLayer = new VectorLayer({
-    map: this.getMap() ?? undefined,
-    source: new VectorSource(),
-  });
+  private positionStyle: Style;
+  private accuracyStyle: Style;
 
-  private positionStyle = new Style({
-    image: new CircleStyle({
-      radius: 6,
-      fill: new Fill({
-        color: '#3399CC',
-      }),
-      stroke: new Stroke({
-        color: '#fff',
-        width: 2,
-      }),
-    }),
-  });
+  private featureAddTracker!: EventsKey;
+  private accuracyTracker!: EventsKey;
+  private positionTracker!: EventsKey;
+  private errorTracker!: EventsKey;
 
-  private accuracyStyle = new Style({
-    fill: new Fill({
-      color: 'rgba(51, 153, 204, 0.5)',
-    }),
-  });
+  private isInitialized = false;
 
   constructor(
     interactionName: string,
@@ -56,64 +55,132 @@ class Location extends Interaction {
   ) {
     super();
     this.set('name', interactionName);
-    if (newPositionStyle) this.positionStyle = newPositionStyle;
-    if (newAccuracyStyle) this.accuracyStyle = newAccuracyStyle;
-    this.trackAccuracy;
-    this.trackPosition;
-    this.addFeatureTracker;
-  }
 
-  private trackAccuracy = this.geolocation.on('change:accuracyGeometry', () => {
-    const accuracy = this.geolocation.getAccuracyGeometry();
-    this.accuracyFeature?.setGeometry(accuracy ? accuracy : undefined);
-  });
-
-  private trackPosition = this.geolocation.on('change:position', () => {
-    const coordinates = this.geolocation.getPosition();
-    if (coordinates) {
-      const point = new Point(coordinates);
-      this.positionFeature?.setGeometry(point);
-
-      if (!this.hasZoomedToLocation) {
-        const extent = point.getExtent();
-        this.getMap()?.getView().fit(extent, {
-          maxZoom: 18,
-          duration: 500,
-          easing: easeOut,
-        });
-        this.hasZoomedToLocation = true;
-      }
-    }
-  });
-
-  private addFeatureTracker = this.locationLayer
-    .getSource()
-    ?.on('addfeature', () => {
-      const extent = this.positionFeature?.getGeometry()?.getExtent();
-      console.log(extent);
-      if (extent) this.getMap()?.getView().fit(extent);
+    this.locationLayer = new VectorLayer({
+      source: new VectorSource(),
+      zIndex: Infinity,
     });
 
+    this.positionStyle =
+      newPositionStyle ??
+      new Style({
+        image: new CircleStyle({
+          radius: 6,
+          fill: new Fill({
+            color: '#3399CC',
+          }),
+          stroke: new Stroke({
+            color: '#fff',
+            width: 2,
+          }),
+        }),
+      });
+
+    this.accuracyStyle =
+      newAccuracyStyle ??
+      new Style({
+        fill: new Fill({
+          color: 'rgba(51, 153, 204, 0.5)',
+        }),
+      });
+
+    this.geolocation = new Geolocation({
+      trackingOptions: {
+        enableHighAccuracy: true,
+      },
+      projection: this.getMap()?.getView().getProjection(),
+    });
+  }
+
+  /**
+   * Log location error in the console
+   */
+  private handleError(): void {
+    this.geolocation.on('error', (error) => {
+      console.error(error);
+      this.dispatchEvent(new LocationEvents(LocationEventsType.LOCATION_ERROR));
+    });
+  }
+
+  /**
+   * Track accuracy change and update the accuracy geometry
+   */
+  private trackAccuracy(): void {
+    this.geolocation.on('change:accuracyGeometry', () => {
+      const accuracy = this.geolocation.getAccuracyGeometry();
+      this.accuracyFeature?.setGeometry(accuracy ? accuracy : undefined);
+    });
+  }
+
+  /**
+   * Track position change and update position geometry
+   */
+  private trackPosition(): void {
+    this.geolocation.on('change:position', () => {
+      const coordinates = this.geolocation.getPosition();
+      if (coordinates) {
+        const point = new Point(coordinates);
+        this.positionFeature?.setGeometry(point);
+
+        if (!this.hasZoomedToLocation) {
+          const extent = point.getExtent();
+          this.getMap()?.getView().fit(extent, {
+            maxZoom: 15,
+            duration: 500,
+            easing: easeOut,
+          });
+          this.hasZoomedToLocation = true;
+          this.dispatchEvent(
+            new LocationEvents(LocationEventsType.LOCATION_FOUND)
+          );
+        }
+      }
+    });
+  }
+
+  /**
+   * Enable / disable tracking
+   * @param enable Should the tracking be enabled ?
+   */
   public enableTracking(enable: boolean): void {
     this.geolocation.setTracking(enable);
     this.trackingEnabled = enable;
 
-    if (enable) {
-      this.locationLayer.getSource()?.clear();
-      this.hasZoomedToLocation = false;
-      console.log('ok');
-    } else {
-      console.log('cas else');
-      this.accuracyFeature = new Feature();
-      this.positionFeature = new Feature();
+    this.initializeParameters();
 
+    this.locationLayer.getSource()?.clear();
+    this.hasZoomedToLocation = false;
+
+    unByKey(this.featureAddTracker);
+    unByKey(this.accuracyTracker);
+    unByKey(this.positionTracker);
+    unByKey(this.errorTracker);
+
+    if (enable) {
+      this.positionFeature = new Feature();
       this.positionFeature.setStyle(this.positionStyle);
-      this.positionFeature.setStyle(this.accuracyStyle);
+
+      this.accuracyFeature = new Feature();
+      this.accuracyFeature.setStyle(this.accuracyStyle);
 
       this.locationLayer
         .getSource()
-        ?.addFeatures([this.accuracyFeature, this.positionFeature]);
-      this.hasZoomedToLocation = false;
+        ?.addFeatures([this.positionFeature, this.accuracyFeature]);
+
+      this.trackAccuracy();
+      this.trackPosition();
+      this.handleError();
+    }
+  }
+
+  /**
+   * Initialize location parameters and add the location layer to the map
+   */
+  private initializeParameters(): void {
+    if (!this.isInitialized) {
+      this.geolocation.setProjection(this.getMap()?.getView().getProjection());
+      this.getMap()?.addLayer(this.locationLayer);
+      this.isInitialized = true;
     }
   }
 }
