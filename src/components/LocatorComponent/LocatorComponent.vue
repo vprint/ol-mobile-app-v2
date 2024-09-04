2f9b39ed3353f37d2e9dc23cc14c8137db4af46d
vprint <valentin.printemps@gmail.com>
2024-09-04 11:26:04 +0200

    Gestion du zoom sur une entité

--- src/components/LocatorComponent/LocatorComponent.vue
+++ src/components/LocatorComponent/LocatorComponent.vue
@@ -8,38 +8,126 @@ import { useMapStore } from 'src/stores/map-store';
 // Map imports
 
 // Others imports
-import Location from 'src/plugins/Location';
+import Location, { LocationEventsType } from 'src/plugins/Location';
 import { INTERACTIONS_PARAMS } from 'src/utils/params/interactionsParams';
 import { storeToRefs } from 'pinia';
+import { EventsKey } from 'ol/events';
+import { unByKey } from 'ol/Observable';
 
 // Script
-const { map } = storeToRefs(useMapStore());
-const isTrackingEnabled = ref(false);
-const islocationFound = ref(false);
-const location = new Location(INTERACTIONS_PARAMS.location);
-
-map.value.addInteraction(location);
-
-function setTracking(tracking: boolean): void {
-  location.enableTracking(tracking);
-  isTrackingEnabled.value = tracking;
-  islocationFound.value = false;
+enum TOOLTIP_MESSAGE {
+  ACTIVATE_LOCATION = 'Activate location',
+  FIT_VIEW_TO_LOCATION = 'Fit the view to location',
+  DEACTIVATE_LOCATION = 'Deactivate location',
 }
 
-// @ts-expect-error normal
-location.on('location:found', () => (islocationFound.value = true));
+const { map } = storeToRefs(useMapStore());
+const isLocationEnabled = ref(false);
+const islocationFound = ref(false);
+const isViewCentered = ref(false);
+const tooltipMessage = ref(TOOLTIP_MESSAGE.ACTIVATE_LOCATION);
+
+let locationFoundListener: (EventsKey & EventsKey[]) | undefined;
+let viewModificationListener: (EventsKey & EventsKey[]) | undefined;
+
+const location = new Location(INTERACTIONS_PARAMS.location);
+map.value.addInteraction(location);
+
+/**
+ * Manage the location process
+ */
+function manageLocation(): void {
+  if (!isLocationEnabled.value) {
+    enableTracking();
+  } else {
+    if (isViewCentered.value && islocationFound.value) {
+      disableTracking();
+    }
+
+    if (!isViewCentered.value && islocationFound.value) {
+      fitView();
+    }
+  }
+}
+
+/**
+ * Enable the tracking
+ */
+function enableTracking(): void {
+  location.enableTracking(true);
+  isLocationEnabled.value = true;
+  tooltipMessage.value = TOOLTIP_MESSAGE.DEACTIVATE_LOCATION;
+
+  locationFoundListener = location.on(
+    // @ts-expect-error OL Event error.
+    LocationEventsType.LOCATION_FOUND,
+    () => {
+      islocationFound.value = true;
+      isViewCentered.value = true;
+    }
+  );
+
+  viewModificationListener = location.on(
+    // @ts-expect-error OL Event error.
+    LocationEventsType.VIEW_MODIFICATION,
+    () => {
+      isViewCentered.value = false;
+      tooltipMessage.value = TOOLTIP_MESSAGE.FIT_VIEW_TO_LOCATION;
+    }
+  );
+}
+
+/**
+ * Disable tracking, reset the component variables and clear the event listener
+ */
+function disableTracking(): void {
+  location.enableTracking(false);
+  isLocationEnabled.value = false;
+  islocationFound.value = false;
+  isViewCentered.value = false;
+  if (locationFoundListener && viewModificationListener) {
+    unByKey(locationFoundListener);
+    unByKey(viewModificationListener);
+  }
+  tooltipMessage.value = TOOLTIP_MESSAGE.ACTIVATE_LOCATION;
+}
+
+/**
+ * Fit the view to the location extent
+ */
+function fitView(): void {
+  location.fitViewToLocation(location.addViewChangeListener);
+  isViewCentered.value = true;
+  tooltipMessage.value = TOOLTIP_MESSAGE.DEACTIVATE_LOCATION;
+}
 </script>
 
 <template>
   <q-btn
-    class="icon-weight-thin"
-    :flat="$q.platform.is.desktop"
-    :fab="$q.platform.is.desktop"
-    :round="$q.platform.is.mobile"
-    :square="$q.platform.is.desktop"
-    :icon="isTrackingEnabled ? 'location_on' : 'sym_s_location_on'"
-    :loading="!islocationFound && isTrackingEnabled"
-    @click="setTracking(!isTrackingEnabled)"
+    flat
+    fab
+    square
+    :color="
+      isViewCentered && isLocationEnabled && islocationFound
+        ? 'orange'
+        : 'primary'
+    "
+    :loading="isLocationEnabled && !islocationFound"
+    :icon="
+      isLocationEnabled && islocationFound ? 'location_on' : 'sym_s_location_on'
+    "
+    @click="manageLocation()"
   >
+    <q-tooltip
+      anchor="bottom middle"
+      self="bottom middle"
+      transition-show="scale"
+      transition-hide="scale"
+      :offset="[0, 40]"
+      :delay="1000"
+      style="border-radius: 0"
+    >
+      {{ tooltipMessage }}
+    </q-tooltip>
   </q-btn>
 </template>
